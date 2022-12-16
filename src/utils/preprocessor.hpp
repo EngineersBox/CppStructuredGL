57639919c3a60100810a65dce9edda494efcc674
EngineersBox <toastcraft.info@gmail.com>
2022-12-16 23:20:43 +1100

    Added preprocessor macro NUM_ARGS for defining static constants with variadic macros

--- src/utils/preprocessor.hpp
+++ src/utils/preprocessor.hpp
@@ -5,7 +5,6 @@
 
 #include <tuple>
 
-#define NUM_ARGS(...) _NUM_ARGS(__VA_ARGS__ 0) - 1
-#define _NUM_ARGS(...) (std::tuple_size<decltype(std::make_tuple(__VA_ARGS__))>::value)
+#define NUM_ARGS(...) (std::tuple_size<decltype(std::make_tuple(__VA_ARGS__ 0))>::value - 1)
 
 #endif //STRUCTURED_GL_PREPROCESSOR_HPP
